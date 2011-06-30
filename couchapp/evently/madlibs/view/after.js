883a737fed874829bbdcac282101bb27c8958b00
Seth Croston Barber <seth@0kelvin.net>
2011-06-30 12:34:25 -0700

    Got the basic mad lib functionality working.

--- couchapp/evently/madlibs/view/after.js
+++ couchapp/evently/madlibs/view/after.js
@@ -1,15 +1,4 @@
 function(data) {
-    $('#tokens').formation();
-
-	$('#madlib span').each(function(){
-		var name = $(this).attr('id');
-		$.log(name);
-		var placeholder = name.replace(/[-_]/g, ' ').replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-		$.formation.addInput({required:true,name:name,placeholderValue:placeholder});
-	});
-
-	$.formation.addButton({type:"submit",value:"Lib it!"});
-	
     $('#tokens form').submit(function(){
 		if ($(this).hasClass('hasErrors')) return(false);
 		$(this).find('input[type="text"]').each(function(){
--- couchapp/evently/madlibs/view/data.js
+++ couchapp/evently/madlibs/view/data.js
@@ -1,14 +1,29 @@
 function(data, req) {
-    var madlib = {};
+    var doc = {};
     var db = $$(this).app.db;
 
     db.openDoc(req.id, {
-        success: function(doc) {
-            madlib = doc;
+        success: function(d) {
+            doc = d;
         }
     }, {async: false});
     
-    madlib.html = madlib.madlib.replace(/\{\{[\w-_\d]+\}\}/g, function(id){return '<span id="' + id.replace('{{', '').replace('}}', '') + '"></span>';});
+    // doc.html = doc.madlib.replace(/\{\{[\w-_\d]+\}\}/g, function(id){return '<span id="' + id.replace('{{', '').replace('}}', '') + '">&nbsp;</span>';});
+    doc.html = doc.madlib;
     
-    return madlib;
+    var tokens = doc.madlib.match(/\{\{[\w-_\d]+\}\}/g);
+    
+    if (tokens.length > 0) {
+        doc.tokens = '';
+        for (i in tokens) {
+            var t = tokens[i];
+            var name = t.replace('{{' , '').replace('}}', '');
+            var placeholder = name.replace(/[-_]/g, ' ').replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    		
+            doc.html = doc.html.replace(t, '<span id="' + name + '"></span>');
+            doc.tokens += '<input type="text" name="' + name + '" placeholder="' + placeholder + '">';
+        }
+    }
+    
+    return doc;
 };
