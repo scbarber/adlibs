553e3abb434fd62c19333fb5ab4b998fc5ec6875
Seth Croston Barber <seth@0kelvin.net>
2011-06-30 11:15:17 -0700

    Added a couple of counts to the madlib doc. Renamed the results array. Updated the recent madlibs list display format.

--- couchapp/evently/madlibs/_changes/data.js
+++ couchapp/evently/madlibs/_changes/data.js
@@ -6,6 +6,7 @@ function(data) {
       p = (r.value && r.value.profile) || {};
       p.title = r.value && r.value.title;
       p.id = r.value && r.value._id;
+      p.tags = r.value && r.value.tags;
       return p;
     })
   }
--- couchapp/evently/profile/profileReady/selectors/form/submit.js
+++ couchapp/evently/profile/profileReady/selectors/form/submit.js
@@ -2,7 +2,9 @@ function() {
   var form = $(this);
   var fdoc = form.serializeObject();
   fdoc.created_on = new Date();
-  fdoc.responses = [];
+  fdoc.results = [];
+  fdoc.lib_count = 0;
+  fdoc.votes = 0;
   fdoc.type = 'madlib';
   fdoc.tags = fdoc.tags.split(/,\s*/);
   fdoc.profile = $$("#profile").profile;
