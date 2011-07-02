e59a745e0f14af73ebff2785aff06338fa2f74ca
Seth Croston Barber <seth@0kelvin.net>
2011-07-01 20:03:57 -0700

    Worked up the ability to edit an *adlib now.

--- couchapp/evently/adlibs/view/selectors/div._changes/_changes/after.js
+++ couchapp/evently/adlibs/view/selectors/div._changes/_changes/after.js
@@ -1,3 +1,24 @@
 function(){
     $('#saved-results li a').fancybox();
+
+        $('a.edit').fancybox({
+            'transitionIn': 'elastic',
+            'transitionOut': 'elastic',
+            'autoDimensions': false,
+            'width': '55%',
+            'height': 'auto',
+            'onStart': function() { 
+                var doc = $$('#adlibs').adlib;
+                $('#add-edit-lib [name="title"]').val(doc.title);
+                $('#add-edit-lib [name="adlib"]').val(doc.adlib);
+                $('#add-edit-lib [name="tags"]').val(doc.tags.join(', '));
+                $('#add-edit-lib [type="submit"]').val('Save');
+                $('#add-edit-lib').append('<input type="hidden" name="_id" value="' + doc._id + '">');
+            },
+            'onClosed': function() {
+                $('form#add-edit-lib')[0].reset();
+                $('#add-edit-lib input[name="_id"]').remove();
+                $('#add-edit-lib input[type="submit"]').val('Add');
+            }
+        })
 }
--- couchapp/evently/profile/profileReady/selectors/form/submit.js
+++ couchapp/evently/profile/profileReady/selectors/form/submit.js
@@ -1,13 +1,29 @@
 function() {
   var form = $(this);
   var fdoc = form.serializeObject();
-  fdoc.created_on = new Date();
-  fdoc.results = [];
-  fdoc.lib_count = 0;
-  fdoc.votes = 0;
-  fdoc.type = 'adlib';
+  var edit = false; // Simple flag to know if we are editing or not
+  
+  if (fdoc._id) {
+      edit = true;
+      var edits = form.serializeObject();
+      $$(this).app.db.openDoc(edits._id, {
+          success: function(d) {
+              fdoc = d;
+              fdoc.title = edits.title;
+              fdoc.adlib = edits.adlib;
+              fdoc.tags = edits.tags;
+          }
+      }, {async: false})
+  } else {
+      fdoc.created_on = new Date();
+      fdoc.results = [];
+      fdoc.lib_count = 0;
+      fdoc.votes = 0;
+      fdoc.type = 'adlib';
+      fdoc.profile = $$("#profile").profile;
+  }
+  
   fdoc.tags = fdoc.tags.split(/,\s*/);
-  fdoc.profile = $$("#profile").profile;
 
   fdoc.html = fdoc.adlib.replace(/<.*?>/g, '');
   
@@ -32,6 +48,14 @@ function() {
     success : function() {
       form[0].reset();
       $.fancybox.close();
+      $('#add-edit-lib input[name="_id"]').remove();
+      $('#add-edit-lib input[type="submit"]').val('Add');
+      
+      if (edit) {
+          $('#adlibs h2').html(fdoc.title);
+          $('#adlibs #tokens form').html(fdoc.tokens);
+          $('#adlibs article').html(fdoc.html);
+      }
     }
   });
   return false;
